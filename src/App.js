65daf610e53430721d27a4de52289f49f2327853
Jarv1sGH <96106893+Jarv1sGH@users.noreply.github.com>
2022-06-20 20:16:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
 import { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
-import About from './Components/About'; 
+// import About from './Components/About'; 
 import TextForm from './Components/TextForm'; 
 import Alert from './Components/Alert';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+// } from "react-router-dom";
 
 function App() {
   const [mode, setmode] = useState('light'); //Wether dark mode is enabled or not 
@@ -40,20 +40,21 @@ function App() {
 
   return (
     <>
-    <Router>
+//     <Router>
     <Navbar title = "TextChanger" AboutText = "About TextChanger" mode={mode}
     toggleMode={toggleMode}/>
     <Alert alert = {alert} />
     <div className="container my-3">
+      <TextForm showAlert = {showAlert}heading = "Enter Text to analyze below "mode={mode}  />
 
-    <Routes>
-          <Route exact path="/"element = {<TextForm showAlert = {showAlert}heading = "Enter Text to analyze below "mode={mode}  />}/>
-          <Route exact path="/about" element = {<About />}/>
+//     <Routes>
+//           <Route exact path="/"element = {<TextForm showAlert = {showAlert}heading = "Enter Text to analyze below "mode={mode}  />}/>
+//           <Route exact path="/about" element = {<About />}/>
             
           
-     </Routes>
+//      </Routes>
     </div>
-    </Router>
+//     </Router>
     </> 
   );
 }
