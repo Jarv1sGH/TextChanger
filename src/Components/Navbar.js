05b396241d9463e19696257dbd220125719bdf62
Jarv1sGH <96106893+Jarv1sGH@users.noreply.github.com>
2022-06-20 20:19:38 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,17 +6,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <a className="navbar-brand" href="#">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">{props.AboutText}</Link>
+          <a className="nav-link" href="/about">{props.AboutText}</a>
         </li> 
       </ul>
       {/* <form className="d-flex">
@@ -35,10 +35,10 @@ export default function Navbar(props) {
 
 Navbar.propTypes = {
     title: PropTypes.string.isRequired,
-    AboutText: PropTypes.string.isRequired
+//     AboutText: PropTypes.string.isRequired
 }
 // for setting defaults in place in case no value is set
 Navbar.defaultProps = {
     title: 'Enter your title here',
-    AboutText: 'Enter AboutText here'
+//     AboutText: 'Enter AboutText here'
 }
