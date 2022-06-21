c419e1e8524ff23f9004406aec8777248b75960c
Jarv1sGH <96106893+Jarv1sGH@users.noreply.github.com>
2022-06-21 22:19:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-// import logo from './logo.svg';
+
 import { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 // import About from './Components/About'; 
 import TextForm from './Components/TextForm'; 
 import Alert from './Components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
-
 function App() {
   const [mode, setmode] = useState('light'); //Wether dark mode is enabled or not 
   const [alert, setAlert] = useState(null);
@@ -40,20 +34,11 @@ function App() {
 
   return (
     <>
-//     <Router>
     <Navbar title = "TextChanger"  mode={mode} toggleMode={toggleMode}/>
     <Alert alert = {alert} />
     <div className="container my-3">
       <TextForm showAlert = {showAlert}heading = "Enter Text to analyze below "mode={mode}  />
-
-//     <Routes>
-//           <Route exact path="/"element = {<TextForm showAlert = {showAlert}heading = "Enter Text to analyze below "mode={mode}  />}/>
-//           <Route exact path="/about" element = {<About />}/>
-            
-          
-//      </Routes>
     </div>
-//     </Router>
     </> 
   );
 }
